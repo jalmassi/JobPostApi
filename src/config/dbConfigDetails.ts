6998a52e7a7957ac36951d8d4ea2cbf00a91751c
jalmassi <justin.almassi@gmail.com>
2021-12-01 00:14:50 -0500

    pw

--- src/config/dbConfigDetails.ts
+++ src/config/dbConfigDetails.ts
@@ -2,4 +2,4 @@ export const dbClient: string = "mysql";
 export const dbName: string = "JobPostDB";
 export const host: string = "localhost";
 export const dbUsername: string = "root";
-export const dbPassword: string = "Forget14";
+export const dbPassword: string = "mysql_password";
